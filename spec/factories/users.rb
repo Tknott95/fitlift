d9d5c9f31cb4ba3815eb0a154c4c59cc135dc225
Trevor Knott <Tknott95@hotmail.com>
2016-04-24 20:12:04 -0600

    Generated User model  *Working guard!

---
+++ spec/factories/users.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :user do
+    username "MyString"
+    first_name "MyString"
+    last_name "MyString"
+    email "MyString"
+    password_digest "MyString"
+  end
+end
