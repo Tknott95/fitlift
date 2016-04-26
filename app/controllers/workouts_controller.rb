0cd8e67642398fffea470a697c2e813db0f1f03d
Trevor Knott <Tknott95@hotmail.com>
2016-04-25 21:29:56 -0600

    Added workouts to post 	*redirects to user_profile after 	*saves the workout but doesn’t show it on profile

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,4 +1,46 @@
 class WorkoutsController < ApplicationController
-def index
-end
+
+  def index
+    @workouts = Workout.all
+  end
+
+  def show
+  end
+
+  def new
+    @workout = Workout.new
+  end
+
+  def create
+    @workout = Workout.new(workout_params)
+
+    if @workout.save
+      redirect_to user_profile_path(current_user),
+      notice: "Thank you for posting your workout #{@user.first_name.titlecase}"
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def workout_params
+    params.require(:workout).permit(
+                              :date, :workout,
+      :motivation_level, :length_lifting, :length_hitting_cardio
+    )
+  end
+
+  def find_workout
+  end
+
 end
